a5839fe1cb853a68b1d831ffbd3f255bd22307cf
Lucas <mariolucas.2000@outlook.com>
2022-11-08 14:34:41 -0300

    :sparkles: fix: beckend

--- frontweb/src/components/NavBar/index.jsx
+++ frontweb/src/components/NavBar/index.jsx
@@ -1,35 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import carrinho from "../../assets/img/compra.svg";
-import { context } from "../../context";
-import { DivRight, Header, Nav, Ul } from './style';
+import { context } from "../../context/index";
+import "./style.css";
+
 
 export const NavBar = () => {
   const { productsCart } = useContext(context);
+
+  const [isMobile, setMobile] = useState(false)
     
   return (
-    <Header>
-        <span>SerraCommerce</span>
-      <Nav>
-        <Ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/catalogo">Catalogo</Link>
-          </li>
-          <li>
-            <Link to="/admin/produto">Admin</Link>
-          </li>
-        </Ul>
-      </Nav>
-        <DivRight>
-          <Link to="/catalogo/carrinho">
-            <img src={carrinho} alt="" />
-            <p>Carrinho ({productsCart.length})</p>
-          </Link>
-        </DivRight>
-    </Header>
+    <nav className="navbar">
+            <h3 className="logo">Serracomerce</h3>
+            <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+            onClick={() => setMobile(false)}>
+                <Link to="/" className="home">
+                    <li>Home</li>
+                </Link>
+                <Link to="/catalogo" className="catalogo">
+                    <li>Catalogo</li>
+                </Link>
+                <Link to="/admin/produto" className="admin">
+                    <li>Admin</li>
+                </Link>
+                <Link to= "/catalogo/carrinho"className="carrinho">
+                {/* <img src={carrinho} alt="" /> */}
+                <p>Carrinho ({productsCart.length})</p>
+                </Link>
+            </ul>
+            <button className="mobile-menu-icon" onClick={() => setMobile(!isMobile)}>
+                {isMobile ? (<i className="fas fa-times"><FaTimes/></i>) : (<i className="fas fa-bars"><FaBars/></i>)}
+            </button>
+        </nav>
     )
 };
-
